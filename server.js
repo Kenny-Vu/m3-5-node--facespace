28c4dd21c976414377757333f3317fe4f5509e26
Kenny Vu <kennyvu@live.ca>
2020-07-08 12:11:58 -0400

    Exercises 1&2 done!

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const { users } = require('./data/users');
+const { users } = require("./data/users");
 
 let currentUser = {};
 
@@ -11,18 +11,49 @@ let currentUser = {};
 const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
 };
+// homepage
+const handleHomepage = (req, res) => {
+  res.status(200);
+  res.render("pages/homepage", { users: users });
+};
+// user page
+const handleProfilePage = (req, res) => {
+  const id = req.params.id;
+  let index = null;
+  users.forEach((user) => {
+    if (id === user._id) {
+      index = users.indexOf(user);
+    }
+  });
+  if (index !== null) {
+    const user = users[index];
+    const friendList = [];
+    users.forEach((possibleFriend) => {
+      user.friends.forEach((friend) => {
+        if (friend === possibleFriend._id) {
+          friendList.push(possibleFriend);
+        }
+      });
+    });
+    console.log(friendList);
+    res.render("pages/profile", { user, friendList });
+  } else {
+    res.status(404).send("I couldn't find what you're looking for.");
+  }
+};
 
 // -----------------------------------------------------
 // server endpoints
 express()
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
-
+  .get("/", handleHomepage)
+  .get("/users/:id", handleProfilePage)
   // a catchall endpoint that will send the 404 message.
-  .get('*', handleFourOhFour)
+  .get("*", handleFourOhFour)
 
-  .listen(8000, () => console.log('Listening on port 8000'));
+  .listen(8000, () => console.log("Listening on port 8000"));
